c43ae07b91c95ce7223a410c7dfec4531b748f6d
vishalkanade1 <77057206+vishalkanade1@users.noreply.github.com>
2023-02-22 17:46:21 +0530

    minor changes

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(() => {
+  console.log("server started.")
+})
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
